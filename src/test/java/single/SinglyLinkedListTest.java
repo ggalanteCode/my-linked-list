35666385df5e80a412951652556e0ea18192d1a8
ggalanteCode <ggalante3@gmail.com>
2025-03-14 10:46:11 +0100

    new unit test for singly list with two elements

--- src/test/java/single/SinglyLinkedListTest.java
+++ src/test/java/single/SinglyLinkedListTest.java
@@ -13,4 +13,12 @@ class SinglyLinkedListTest {
         assertEquals(list.getSize(), 1);
     }
 
+    @Test
+    void givenASinglyLinkedList_whenInsertingTwoElements_thenSizeIsTwo() {
+        SinglyLinkedList<Integer> list = new SinglyLinkedList<>();
+        list.add(1);
+        list.add(2);
+        assertEquals(list.getSize(), 2);
+    }
+
 }
